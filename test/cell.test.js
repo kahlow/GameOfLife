94ce2fdbbc68dfa225677442061f3b78935bd8ee
Tommy Kahlow <tkahlow@gmail.com>
2012-05-06 19:32:55 -0400

    Updated tests

--- test/cell.test.js
+++ test/cell.test.js
@@ -2,20 +2,21 @@ $(document).ready(function(){
 	module("Cells");
 
 	test("Cell Death", function(){
-		ok(true, "This cell is dead.");
-		Cell.Dead();		
-		equal(Cell.Status, "Dead", "We expect death to be true");
+		var cell = new Cell();
+        cell.Dead();
+        equal(cell.Status, "Dead", "We expect death to be true");
 	});
 
 	test("Cell Life", function(){
-		ok(true, "This cell is alive.");
-		Cell.Alive();
-		equal(Cell.Status, "Alive", "We expect alive to be true");
+		var cell = new Cell();
+        cell.Alive();
+		equal(cell.Status, "Alive", "We expect alive to be true");
 	});
 
 	test("Cell location", function(){
-		Cell.SetLocation(10,20);
-		equal(10, Cell.X, "We expect X to be 10");
-		equal(20, Cell.Y, "We expect Y to be 20");
+        var cell = new Cell();
+		cell.SetLocation(10,20);
+		equal(10, cell.X, "We expect X to be 10");
+		equal(20, cell.Y, "We expect Y to be 20");
 	});
 });
