3380a757c76c7102686d7ce956dbebd02e5f4a80
Tommy Kahlow <tkahlow@gmail.com>
2012-05-10 22:59:42 -0400

    Updated and added better tests

--- test/game.test.js
+++ test/game.test.js
@@ -10,16 +10,16 @@ $(document).ready(function(){
 	module("Game Logic");
 	
 	test("Board Creation", function(){
-        var gameBoard = new Game();
-        var board = gameBoard.createBoard(25,25,25);
-	    equal(25, board.length, "We expect board width to be 25");
-        equal(25, board[0].length, "I expect boards height to be 25");
-        
+        var game = new Game();
+        game.createBoard(25,25,25);
+	    equal(25, game.gameBoard.length, "We expect board width to be 25");
+        equal(25, game.gameBoard[0].length, "I expect boards height to be 25");
+                
         var alive = 0;
 
         for (var i = 0; i < 25; i++){
             for (var j = 0; j < 25; j++){
-                if (board[i][j].status == "Alive"){
+                if (game.gameBoard[i][j].status == "Alive"){
                     alive++;
                 }
             }
@@ -29,54 +29,50 @@ $(document).ready(function(){
     });
 
     test("Game Generation", function(){
-        var gameBoard = new Game();
-        var board = gameBoard.createBoard(5,5,25);
-        var nextBoard = gameBoard.nextGeneration(board);
+        var game = new Game();
+        var newBoard = game.createBoard(5,5,25);
+        var nextBoard = game.nextGeneration();
        
-        notEqual(board, nextBoard, "I expect the two boards to be different");
+        notEqual(newBoard, nextBoard, "I expect the two boards to be different");
     });
 
     test("Rule 1: Fewer than two neighbors equals death", function(){
         var game = new Game();
-        var board = game.createBoard(3,3,0);
-        board[1][1].alive();
-        var newBoard = game.nextGeneration(board);
+        game.createBoard(3,3,0);
+        game.gameBoard[1][1].alive();
+        game.nextGeneration();
 
-        equal(newBoard[1][1].status, "Dead", "I expect the cell to be dead");
+        equal(game.gameBoard[1][1].status, "Dead", "I expect the cell to be dead");
     });
     
     test("Rule 2: Two neighbors equals continued life", function(){
         var game = new Game();
-        var board = game.createBoard(3,3,0);
-        board[1][1].alive();
-        board[1][2].alive();
-        board[0][2].alive();
-        var newBoard = game.nextGeneration(board);
+        game.createBoard(3,3,0);
+        game.gameBoard[1][0].alive();
+        game.gameBoard[1][1].alive();
+        game.gameBoard[1][2].alive();
+        game.nextGeneration();
         
-        equal(newBoard[1][1].status, "Alive", "I expect the cell to be alive");
+        equal(game.gameBoard[1][1].status, "Alive", "I expect the cell to be alive");
     });
     
     test("Rule 3: More than three neighbors equals death", function(){
         var game = new Game();
-        var board = game.createBoard(3,3,0);
-        board[1][1].alive();
-        board[1][2].alive();
-        board[0][2].alive();
-        board[2][1].alive();
-        var newBoard = game.nextGeneration(board);
+        var board = game.createBoard(3,3,9);
+        game.nextGeneration();
 
-        equal(newBoard[1][1].status, "Dead", "I expect the cell to be dead");
+        equal(game.gameBoard[1][1].status, "Dead", "I expect the cell to be dead");
     });
     
     test("Rule 4: Any dead cell with three neighbors becomes a live cell", function(){
         var game = new Game();
-        var board = game.createBoard(3,3,0);
-        board[1][1].dead();
-        board[1][2].alive();
-        board[2][2].alive();
-        board[2][1].alive();
-        var newBoard = game.nextGeneration(board);
+        game.createBoard(3,3,0);
+        game.gameBoard[1][1].dead();
+        game.gameBoard[1][2].alive();
+        game.gameBoard[2][2].alive();
+        game.gameBoard[2][1].alive();
+        game.nextGeneration();
 
-        equal(newBoard[1][1].status, "Alive", "I expect the cell to become alive");
+        equal(game.gameBoard[1][1].status, "Alive", "I expect the cell to become alive");
     });
 });
