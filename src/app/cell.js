ecfb2b78fdb15a78787739fa8c60bcc7cca3ab79
Tommy Kahlow <tkahlow@gmail.com>
2012-05-10 22:58:35 -0400

    fixed text space formatting I hope

--- src/app/cell.js
+++ src/app/cell.js
@@ -1,19 +1,19 @@
 var Cell = Backbone.Model.extend({
-    x: 0,
+	x: 0,
 	y: 0,
 	status: "Dead",
 	neighbors: [],
 	setLocation: function(x,y){
-					this.x = x;
-					this.y = y;  
+				this.x = x;
+				this.y = y;
 			},
-    alive: function(){
+	alive: function(){
 				this.status = "Alive";
 			},
 	dead: function(){
 				this.status = "Dead";
-		  	},
+			},
 	setNeighbors: function(n){
 				this.neighbors = n;
-			},	
+			},
 });
